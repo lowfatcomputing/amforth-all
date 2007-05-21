0dbae7c667aff6cc05247e37296b887a2f3bc0b9
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2007-05-21 18:53:09 +0000

    simple template project
    git-svn-id: svn://svn.code.sf.net/p/amforth/code@313 fd1c0b69-5d68-47f5-910c-86182ebb6ecd

---
+++ appl/template/template.asm
@@ -0,0 +1,41 @@
+; This is a template for an amforth project.
+; The order of the entries must not be changed since
+; it is very important to the settings in the right order
+;
+; first is to include the macros from the amforth
+; directory 
+.include "macros.asm"
+
+; amforth needs two essential parameters
+; cpu clock in hertz
+.equ cpu_frequency = 8000000
+; initial baud rate of terminal
+.equ baud_rate = 9600
+
+; the optional dictionary can
+;   - not be included, set to 0 (tero)
+;   - be included in the rww section: set to 1 (one)
+;   - be included in the nrww (bootsector) area: set to 2 (two)
+; the optional dictionary can be quite large so putting
+; it into the bootsector area does make sense for the bigger
+; atmeags only (ATmega32 and up)
+
+.set dict_optional=1
+
+; include the amforth device definition file. These
+; files include the *def.inc from atmel internally.
+.include "devices/atmega169.asm"
+
+; change these settings only if you know what you do.
+  .set heap = ramstart
+  .set VE_HEAD = $0000
+
+; an device specific initialization which cannot
+; be implemented in forth with it's TURNKEY vector.
+
+.org codestart
+device_init:
+    ret
+
+; include the whole source tree.
+.include "amforth.asm"
